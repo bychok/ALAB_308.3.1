2b891b6d4e13a6faa406b18961cb0f5723ce622c
Vadim J. Bychok <vadim@bychok.net>
2024-03-15 08:26:38 -0700

    adds solution to Part 1: Fizz Buzz

---
+++ app.js
@@ -0,0 +1,32 @@
+// Part 1: Fizz Buzz
+/*
+Accomplish the following:
+Loop through all numbers from 1 to 100.
+If a number is divisible by 3, log “Fizz.”
+If a number is divisible by 5, log “Buzz.”
+If a number is divisible by both 3 and 5, log “Fizz Buzz.”
+If a number is not divisible by either 3 or 5, log the number.
+Remember to commit your solution once it is working.
+*/
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 == 0 && i % 5 == 0) {
+    console.log("Fizz Buzz");
+  } else if (i % 3 == 0) {
+    console.log("Fizz");
+  } else if (i % 5 == 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
+}
+
+// Part 2: Prime Time
+/*
+Write a script that accomplishes the following:
+Declare an arbitrary number, n.
+Create a loop that searches for the next prime number, starting at n and incrementing from there.
+As soon as you find the prime number, log that number and exit the loop.
+Continuing with the example above, if n is equal to 4,
+your loop should log 5. Similarly, if n is 5, it should log 7, and if n is 9, it should log 11. 
+*/
